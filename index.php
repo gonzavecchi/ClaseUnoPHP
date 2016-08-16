095fb0315422f5b2ce426ec728e3e466d05d3745
gonzavecchi <gonzavecchi@gmail.com>
2016-08-16 20:27:19 -0300

    Agrego "El cambio"

--- index.php
+++ index.php
@@ -5,14 +5,18 @@
 <body>
 <H1>Hola Mundo</H1>
 <?PHP
+
 //echo "Hola PHP";
 //echo "<H2> Hola PHP <H2>";
 
 $Numero = 10;
 
-echo "El número es: " . $Numero . "<br>";
-echo "El número es: $Numero <br>";
+//echo "El número es: " . $Numero . "<br>";
+//echo "El número es: $Numero <br>";
+
+echo "El cambio.";
 
 ?>
+
 </body>
 </html>
