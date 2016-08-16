e01ea619d953e7f3eaf461739df7a64894e297ff
gonzavecchi <gonzavecchi@gmail.com>
2016-08-16 20:24:36 -0300

    Subo el "INDEX.PHP" para realizar el primer commit de prueba.

---
+++ index.php
@@ -0,0 +1,18 @@
+<html>
+<head>
+	<title>Vecchi</title>
+</head>
+<body>
+<H1>Hola Mundo</H1>
+<?PHP
+//echo "Hola PHP";
+//echo "<H2> Hola PHP <H2>";
+
+$Numero = 10;
+
+echo "El número es: " . $Numero . "<br>";
+echo "El número es: $Numero <br>";
+
+?>
+</body>
+</html>
